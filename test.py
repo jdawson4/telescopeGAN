c7370f3cf110f17b49144958e8f212b41293e0c5
Jacob Dawson <jacob.dawson@me.com>
2023-06-09 11:58:22 -0400

    change test file

--- test.py
+++ test.py
@@ -9,27 +9,48 @@ from astropy.io import fits
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-original = tf.constant([[[1.0], [2.0], [3.0]]])
-print(original.shape)
 
-with fits.open('example_data/raw_jupiter.fits') as hdul:
-    # using "with open" works with astropy
+def processFile(path, filename):
+    with fits.open(path) as hdul:
+        dataFound = False
+        for i in range(2):
+            h = hdul[i]
+            if h.data is None:
+                continue
+            if len(h.data.shape) == 2:
+                dataFound = True
+                data = h.data
+                break
+        if not dataFound:
+            print("DATA NOT FOUND AT", path)
+            return
+        # print(np.amax(data)) # 4154.5635
+        # print(np.amin(data)) # -1340.3322
+        # print(np.mean(data)) # 658.7233
+        data = data.astype(np.float32)
+        data = np.nan_to_num(data)
+        data = np.clip(data, np.percentile(data, 20.0), np.percentile(data, 99.625))
+        if np.amin(data) < 0.0:
+            data += np.amin(data)
+        if np.amin(data) > 0.0:
+            data -= np.amin(data)
+        data /= np.amax(data)
+        data *= 255.0
 
-    #hdul.info()
-    data = hdul[1].data
-    #print(np.amax(data)) # 47607.387
-    #print(np.amin(data)) # -96.857056
-    #print(np.mean(data)) # 222.7446
+        testData = tf.constant(data, shape=(len(data), len(data[1]), 1))
+        print(testData.shape)
+        # imgplot = plt.imshow(data, cmap="gray")
+        # plt.show()
 
-    # let's make a greyscale between 0-255
-    data = np.clip(data, np.percentile(data, 20.0), np.percentile(data, 99.625))
-    if (np.amin(data) < 0.0):
-        data += np.amin(data)
-    #data = np.log(data)
-    data /= np.amax(data)
-    data *= 255.0
-    testData = tf.constant(data, shape=(len(data),len(data[1]),1))
-    print(testData.shape)
-    imgplot = plt.imshow(data, cmap="gray")
-    plt.show()
+
+list_of_files = {}
+for dirpath, dirnames, filenames in os.walk("example_data"):
+    for filename in filenames:
+        if filename.endswith(".fits"):
+            list_of_files[filename] = os.sep.join([dirpath, filename])
+
+for k, v in list_of_files.items():
+    processFile(v, k)
+    break
